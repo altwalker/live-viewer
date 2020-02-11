1fca46376c0b77848ae05f34d6c20c6241422d84
Robert Dezmerean <robert.dezmerean@altom.com>
2020-02-11 17:38:10 +0200

    Add walk command

--- example/tests/test.py
+++ example/tests/test.py
@@ -1,3 +1,4 @@
+import time
 import string
 import random
 
@@ -11,48 +12,63 @@ class ModelName:
 
     def edge_A(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_B(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_C(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_D(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_E(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_F(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_G(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def edge_H(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_A(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_B(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_C(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_D(self):
         print(genreate_random_message())
+        time.sleep(3)
 
         if random.randint(0, 10) > 8:
             raise Exception(genreate_random_message())
 
     def vertex_E(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_F(self):
         print(genreate_random_message())
+        time.sleep(3)
 
     def vertex_G(self):
         print(genreate_random_message())
+        time.sleep(3)
