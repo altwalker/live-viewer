9fbe2cb0e8c58f9a3c92cc5b76ec7fd1517f1a98
Robert-96 <dezmereanrobert@gmail.com>
2023-09-19 22:30:51 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ require('./js/index.js');
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
       console.log('SW registered: ', registration);
     }).catch(registrationError => {
       console.log('SW registration failed: ', registrationError);
